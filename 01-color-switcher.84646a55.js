1daaaa3ac6ee5120c7c6e1d5c3d20127be7a6cb0
ValeriyVinn <115177117+ValeriyVinn@users.noreply.github.com>
2023-04-06 19:20:28 +0000

    Deploying to gh-pages from @ ValeriyVinn/goit-js-hw-09@486c4a3d08df9ffc8ff2862c87a737d37ac3aa97 🚀

---
+++ 01-color-switcher.84646a55.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");t.addEventListener("click",(function(){t.disabled=!0,o.disabled=!1,e=setInterval(n,1e3)})),o.addEventListener("click",(function(){t.disabled=!1,o.disabled=!0,clearInterval(e)}));var e=null;function n(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));console.log("Color = ",t),document.body.style.backgroundColor=t}console.dir(t)}();
+//# sourceMappingURL=01-color-switcher.84646a55.js.map
