6a435256bd7ab2fa7c448ab672a6abaf7f8fb60e
Valeriy <valeriy.all@gmail.com>
2023-04-08 22:55:08 +0300

    Task 3 Promises

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -6,3 +6,48 @@ function createPromise(position, delay) {
     // Reject
   }
 }
+
+import Notiflix from 'notiflix';
+
+const formSubmit = document.querySelector('.form');
+
+formSubmit.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const amount = event.currentTarget.amount.valueAsNumber;
+  const step = event.currentTarget.step.valueAsNumber;
+  let delay = event.currentTarget.delay.valueAsNumber;
+
+  for (let currentPosition = 1; currentPosition <= amount; currentPosition++) {
+    createPromise(currentPosition, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `Rejected promise ${position} in ${delay}ms`
+        );
+      });
+    delay += step;
+  }
+
+  function createPromise(position, delay) {
+    const shouldResolve = Math.random() > 0.3;
+
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve({ position, delay });
+        } else {
+          reject({ position, delay });
+        }
+      }, delay);
+    });
+  }
+
+  event.currentTarget.reset();
+}
